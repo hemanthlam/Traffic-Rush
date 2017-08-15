2714480e6cde15edb87feedbd7f306a271a7bf38
NikhithaDurvasula <nikhithadurvasula69@gmail.com>
2017-08-15 04:19:30 -0700

    Update SplashScreen.java

--- app/src/main/java/com/opensource/trafficrush/SplashScreen.java
+++ app/src/main/java/com/opensource/trafficrush/SplashScreen.java
@@ -2,9 +2,12 @@ package com.opensource.trafficrush;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SplashScreen extends Activity {
 
@@ -12,18 +15,16 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Button login = (Button) findViewById(R.id.Login);
 
-        int SPLASH_DELEY = 1000;
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
                 Intent intent = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, SPLASH_DELEY);
+        });
     }
-
-
-
 }
